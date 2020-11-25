97d6d00da0c369bdf12dfe217abc003682c45f38
pro-ifaz <ifazahmed98@gmail.com>
2020-11-25 08:19:32 +0600

    recalled

---
+++ script.js
@@ -0,0 +1,59 @@
+//login-button eventHandler
+const loginBtn = document.getElementById('login');
+loginBtn.addEventListener('click', function () {
+    const loginArea = document.getElementById('login-area');
+    loginArea.style.display = "none";
+    const transactionArea = document.getElementById("transaction-area");
+    transactionArea.style.display = "block";
+})
+
+//deposit-button eventHandler
+const depositBtn = document.getElementById("addDeposit");
+depositBtn.addEventListener("click", function () {
+   const depositNumber = getInputNumber("depositAmount");
+
+    currentDeposit = document
+        .getElementById("currentDeposit")
+        .innerText;
+    const currentDepositNumber = parseFloat(currentDeposit);
+    const totalDeposit = depositNumber + currentDepositNumber;
+
+    document
+        .getElementById("currentDeposit")
+        .innerText = totalDeposit;
+
+    document
+        .getElementById("depositAmount")
+        .value = "";
+        updateSpanText("currentBalance", depositNumber);
+})
+
+// withdraw button handler
+
+const withdrawBtn = document.getElementById("addWithdraw");
+withdrawBtn.addEventListener("click",function(){
+    
+    const withdrawNumber = getInputNumber("withdrawAmount");
+    
+    updateSpanText("currentWithdraw", withdrawNumber);
+    updateSpanText("currentBalance", -1 * withdrawNumber );
+    document.getElementById("withdrawAmount").value = "";    
+
+})
+
+function getInputNumber (id) {
+    const amount = document.getElementById(id).value;
+    const amountNumber = parseFloat(amount);
+    return amountNumber;
+}
+
+function updateSpanText(id, depositNumber) {
+    const currentBalance = document
+        .getElementById(id)
+        .innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+    const totalBalance = depositNumber + currentBalanceNumber;
+    document
+        .getElementById(id)
+        .innerText = totalBalance;
+}
